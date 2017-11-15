c3ac2534cdcb94623398f19f0b0f8938874330b5
THIAGODOUGLAS-P\Thiago Douglas <tiago.douglas@smn.com.br>
2017-11-15 17:47:24 -0200

    bootstrap added

--- web-store/src/app/app.module.ts
+++ web-store/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidenavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
